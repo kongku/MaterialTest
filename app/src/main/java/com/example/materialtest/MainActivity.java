95e32cacc422e2c4a63a9ff1e577fb03641ad9df
kongku <kongku@gmail.com>
2017-06-06 14:40:41 +0800

    snackbar

--- app/src/main/java/com/example/materialtest/MainActivity.java
+++ app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.materialtest;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,14 +22,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//必须设置这一步
         ActionBar actionBar=getSupportActionBar();
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        FloatingActionButton fab= (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                可以新建Snackbar的实例，然后获取它的view对象，就可以设置其他属性，如背景颜色
+//                snackbar.getView().setBackgroundColor(colorId);
+                //使用snackbar时注意，要想把键盘隐藏掉，Keyboard.hide()，不然会被键盘遮挡住
+                //还有，SnackBar的实现方式其实是addView，因此，它的父控件不能是ScrollView这样只允许有一个子控件的控件。
+                //使用技巧，让触发的这个view的父控件是CoordinatorLayout就可以了，并支持右划取消
+                Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Data restored",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
         NavigationView navigationView= (NavigationView) findViewById(R.id.nav_view);
         navigationView.setCheckedItem(R.id.nav_call);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
